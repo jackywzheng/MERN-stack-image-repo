ad5158750c73a7a942dabc43e07dee22198593f2
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2020-09-13 16:24:26 -0700

    Complete basic login and signup component using card

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Shopigram</h2>
+        <input type="text" placeholder="E-mail" />
+        <input type="text" placeholder="Password" />
+        <button className="btn waves-effect waves-light #1976d2 blue darken-2">
+          Login
+        </button>
+        <h5>
+          <Link to="/signup">Don't have an account?</Link>
+        </h5>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <h1>Sign up</h1>;
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Shopigram</h2>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="E-mail" />
+        <input type="text" placeholder="Password" />
+        <button className="btn waves-effect waves-light #1976d2 blue darken-2">
+          Sign up
+        </button>
+        <h5>
+          <Link to="/login">Already have an account?</Link>
+        </h5>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
