ac0f8b5ba44530ce5a264abffe637e4a0c2f8eeb
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2020-09-13 15:31:11 -0700

    Finish most of backend. Begin frontend work using React

--- server/routes/post.js
+++ server/routes/post.js
@@ -2,8 +2,34 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const requireLogin = require("../middleware/requireLogin");
+const { request } = require("express");
 const Post = mongoose.model("Post");
 
+// Show all posts created by all users
+router.get("/allpost", (request, response) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .then((posts) => {
+      response.json({ posts });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// Show all posts created by user
+router.get("/mypost", requireLogin, (request, response) => {
+  Post.find({ postedBy: request.user._id })
+    .populate("postedBy", "_id name")
+    .then((mypost) => {
+      response.json({ mypost });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// Create post
 router.post("/createpost", requireLogin, (request, response) => {
   const { title, body } = request.body;
   if (!title || !body) {
