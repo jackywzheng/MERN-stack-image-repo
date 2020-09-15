e57b11dac2056660f8aa5ba644296d2097514983
Jacky Zheng <30736593+jackywzheng@users.noreply.github.com>
2020-09-14 19:39:39 -0700

    Complete CreatePost component

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Jacky</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1587930734782-4fe289d9ea8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite</i>
+          <h6>Post Title</h6>
+          <p>Post description</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Jacky</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1587930734782-4fe289d9ea8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite</i>
+          <h6>Post Title</h6>
+          <p>Post description</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Jacky</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1587930734782-4fe289d9ea8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+        </div>
+        <div className="card-content">
+          <i className="material-icons">favorite</i>
+          <h6>Post Title</h6>
+          <p>Post description</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
